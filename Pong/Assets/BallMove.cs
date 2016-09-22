18e0457b12ea13ecd2016797e0febb64fcf8f48a
Zale <zalerasco@gmail.com>
2016-09-22 16:35:46 -0400

    Added Everything
    New Project

---
+++ Pong/Assets/BallMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour
+{
+    public float speed = 0;
+
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }  
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.name == "WallBlue")
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(0, 0);
+            StartCoroutine(WaitAfter(-1));
+        }
+        else if(col.gameObject.name == "WallRed")
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(0, 0);
+            StartCoroutine(WaitAfter(1));
+        }
+        if (col.gameObject.name == "RacketLeft")
+        {
+            Vector2 yforce = col.gameObject.GetComponent<Rigidbody2D>().velocity/2;
+            GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity + yforce;
+        }
+        if (col.gameObject.name == "RacketRight")
+        {
+            Vector2 yforce = col.gameObject.GetComponent<Rigidbody2D>().velocity / 2;
+            GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity + yforce;
+        }
+    }
+
+    IEnumerator WaitAfter(float red)
+    {
+        Vector2 dir = new Vector2(red, 0).normalized;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        yield return new WaitForSecondsRealtime(1);
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+    }
+}
---
+++ Pong/Assets/MoveRacket.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+    public float pspeed = 30;
+    public string vaxis = "Vertical";
+    public string haxis = "Horizontal";
+    public int difficulty = 0;
+    bool controlled = false;
+
+    void FixedUpdate() {
+        
+        //Useful stuff
+        Rigidbody2D ballobj = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody2D>();
+        Vector2 ballvel = ballobj.velocity;
+        Vector2 ballcord = ballobj.position;
+        float ballx = ballobj.position.x;
+        float bally = ballobj.position.y;
+        Rigidbody2D me = GetComponent<Rigidbody2D>();
+        Debug.Log(ballobj.velocity.y / ballobj.velocity.x * (me.position.x) + ballobj.position.y);
+        //Player Input
+        float vinput = Input.GetAxisRaw(vaxis);
+        float hinput = Input.GetAxisRaw(haxis);
+        me.velocity = new Vector2(me.velocity.x, vinput * pspeed);
+        //Move sideways???      me.velocity = new Vector2(hinput * speed, me.velocity.y);
+
+        if (vinput != 0)
+        {
+            controlled = true;
+        }
+
+        if (difficulty == 0 && !controlled /*&& ballobj.velocity.x < 0*/)
+        {
+            float difference = me.position.y - ballobj.position.y;
+
+                if (difference < -0.01)
+                {
+                    me.velocity = new Vector2(me.velocity.x, -difference * 8 - 3);
+                }
+                else if (difference > 0.01)
+                {
+                    me.velocity = new Vector2(me.velocity.x, -difference * 8 + 3);
+                }
+        }
+        if (difficulty == 1 && !controlled)
+        {
+            if (me.position.y < ballobj.position.y)
+            {
+                float y = ballobj.velocity.y / ballobj.velocity.x * (ballobj.position.x) + ballobj.position.y;
+
+                me.velocity = new Vector2(me.velocity.x, 1 * pspeed);
+            }
+            else if (me.position.y > ballobj.position.y)
+            {
+                me.velocity = new Vector2(me.velocity.x, -1 * pspeed);
+            }
+        }
+    }   
+}
